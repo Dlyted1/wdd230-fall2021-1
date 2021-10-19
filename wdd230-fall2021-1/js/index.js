efb8042c1266d382d04b00cd0e2370331f633f22
Dlyted1 <mookathy3@gmail.com>
2021-10-19 15:55:53 -0400

    new

--- wdd230-fall2021-1/js/index.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#theyear").textContent = new Date().getFullYear();
-document.querySelector("#lastmod").textContent = document.lastModified;
--- wdd230-fall2021-1/lesson5/js/banner.js
+++
@@ -1,8 +0,0 @@
-
-var now = new Date();
-var CurrentDay = now.getDay();
-
-if ((CurrentDay == 5) {
-    $('#banner').show();
-    } 
-else $('#banner').hide();
