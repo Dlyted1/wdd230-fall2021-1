58f9026fe479f15cbb65f1984e866cd7a43359cd
Dlyted1 <mookathy3@gmail.com>
2021-10-28 18:58:59 -0400

    commit

---
+++ lesson7/js/hamscripts.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+  }
+  // select the element to manipulate (output to)
+  const datefield = document.querySelector("date");
+  
+  // derive the current date using a date object
+  const now = new Date();
+  const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+      now
+  );
+  // long, medium, short options ... try them
+  
+  datefield.textContent = fulldate;
+  
