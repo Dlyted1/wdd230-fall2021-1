f2acd3da7757f19d1fa2a61267da85659a77c582
Dlyted1 <mookathy3@gmail.com>
2021-10-30 17:51:28 -0400

    commit

--- lesson7/js/daysbetween.js
+++ lesson7/js/daysbetween.js
@@ -8,15 +8,15 @@ const lastVisit = localStorage.getItem("lastvisit") || todaysDate;
 
 //determine number of days
      // to round number    .toFixed(0)
-daysbetween = (todaysDate - lastVisit) / millisecondsToDays).toFixed(0);
+daysbetween = ((todaysDate - lastVisit) / millisecondsToDays).toFixed(0);
 
 if (daysbetween == 0) {
-  document.getElementById("lastvisit").innerHTML = "Welcome to your 1st visit!";
+  document.querySelector("lastvisit").innerHTML = `Welcome to your 1st visit!`;
   }
 
   else {
 
-  document.getElementById("lastvisit").innerHTML = "Your last visit was:" + daysbetween ".";
+  document.querySelector("lastvisit").innerHTML = `Your last visit was: ${daysbetween} days.`;
 }
 
   //need to update the stored date
