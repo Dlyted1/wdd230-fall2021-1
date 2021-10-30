faf6f4e1a65a2d2c26ed89ddc9b3c9d6f876738c
Dlyted1 <mookathy3@gmail.com>
2021-10-30 16:38:27 -0400

    commit

--- lesson7/js/daysbetween.js
+++ lesson7/js/daysbetween.js
@@ -1,15 +1,23 @@
 //determine days between stored date and visit date... today
-//determine todays date 
+//determine todays date
+const todaysDate = new Date(); 
 const millisecondsToDays = 8640000;  // divide the difference between dates in milliseconds
 // last time visit... stored  in local storage
    //1.     localStrage.getItem('lastvisit')
 
-const lastVisit = localStorage.getItem("lastvisit"); // || Date.now();  
+const lastVisit = localStorage.getItem("lastvisit") || Date.now(); 
+  if (lastVisit == 0) {
+    message = 'Welcome to your 1st visit!'
+
+  }
+
+  else {
 
 //determine number of days
 
-  (lastvist - Date.now() / millisecondsToDays);   // to round number    .toFixed(0)
-
+  (lastvisit - Date.now() / millisecondsToDays)
+     // to round number    .toFixed(0)
+  }
   //display here like in windchill
   document.getElementById("lastvisit").innerHTML= lastVisit;
 
