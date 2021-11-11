f9e9a11a097929c86b116f242d07c98f9ca6b93d
Dlyted1 <mookathy3@gmail.com>
2021-11-11 18:56:37 -0500

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -3,22 +3,36 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
 
 fetch(requestURL)
   .then(function (response) {
-    return response.json();
+    if (!response.ok) {
+      throw Error(response.statusText);
+    } else {
+      return response.json();
+    }
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    // const prophets = jsonObject['prophets'];
-    // prophets.forEach((prophet) => {
-    // let card = document.createElement('section');
-    // let h2 = document.createElement('h2');
-    //  h2.innerHTML =
-    //  prophet.name +
-    //  ' <span style="color:navy;">' +
-    //  prophet.lastname +
-    //  '</span'; // ` challenge
-    //  card.appendChild(h2);
-    //  document.querySelector('div.cards').appendChild(card);
-    // });  
+    const prophets = jsonObject['prophets'];
+    prophets.forEach((prophet) => {
+     // create the card and each element needed
+     let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let birthplace = document.createElement('p');
+      let birthdate = document.createElement('p');
+      let image = document.createElement('img');
+
+    // now populate the elements with the data from the JSON file
+    // h2.innerHTML = prophet.name + ' <span style="color:navy;">' + prophet.lastname + '</span'; // ` challenge can use template literal here instead see below line to replace above
+    h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}</span`;
+    birthdate.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+    birthplace.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+
+    // build the Prophet cards
+    card.appendChild(h2);
+    card.appendChild(birthdate);
+    card.appendChild(birthplace);
+    card.appendChild(image);
+    document.querySelector('div.cards').appendChild(card);
+   });  
   });
 
   
