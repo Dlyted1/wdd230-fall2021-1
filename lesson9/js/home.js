1afd39b64b3c8d0b65a471ca330b763472dda744
Dlyted1 <mookathy3@gmail.com>
2021-11-13 17:13:23 -0500

    commit

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -1,5 +1,5 @@
 // store the resource, the URL of the JSON file into a const variable to start. //
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -11,26 +11,33 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    prophets.forEach((prophet) => {
+    const towns = jsonObject['towns'];
+    towns.forEach((town) => {
      // create the card and each element needed
      let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let birthplace = document.createElement('p');
-      let birthdate = document.createElement('p');
+      let motto = document.createElement('p');
+      let yearFounded = document.createElement('p');
+      let currentPopulation = document.createElement('p');
+      let averageRainfall = document.createElement('p');
+
       let image = document.createElement('img');
 
     // now populate the elements with the data from the JSON file
     // h2.innerHTML = prophet.name + ' <span style="color:navy;">' + prophet.lastname + '</span'; // `  can use template literal here instead see below line to replace above
-    h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}</span`;
-    birthdate.innerHTML = `Date of Birth: ${prophet.birthdate}`;
-    birthplace.innerHTML = `Place of Birth: ${prophet.birthplace}`;
-    image.setAttribute('src', prophet.imageurl);
-    image.setAttribute('alt', `${prophet.name} ${prophet.lastname}-${prophet.order}`);
+    h2.innerHTML = `${town.name} <span class="highlight">${town.name}</span`;
+    motto.innerHTML = `${town.motto}`;
+    yearFounded.innerHTML = `Year Founded: ${town.yearFounded}`;
+    currentPopulation.innerHTML = `Population: ${town.currentPopulation}`;
+    averageRainfall.innerHTML = `Average Rain Fall: ${town.averageRainfall}`;
+    image.setAttribute('src', town.imageurl);
+    image.setAttribute('alt', `${town.name} ${town.lastname}-${town.order}`);
     // build the Prophet cards
     card.appendChild(h2);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
+    card.appendChild(motto);
+    card.appendChild(yearFounded);
+    card.appendChild(currentPopulation);
+    card.appendChild(averageRainfall);
     card.appendChild(image);
 
     
