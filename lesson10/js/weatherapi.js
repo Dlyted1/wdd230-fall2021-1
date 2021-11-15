5bc7cd2cf2baf537bad205dbd265f76189dca54e
Dlyted1 <mookathy3@gmail.com>
2021-11-15 18:54:45 -0500

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,14 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={86586ace50f6244d589757e7ab590bcf}";
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473units=imperial&appid={86daac23ffd9b9bce45f9ec73da9e4e9}";
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
+  .then(function(response) => response.json() })
+  .then(function(jsObject) => {
     console.log(jsObject);
   });
+
+
+
+  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
