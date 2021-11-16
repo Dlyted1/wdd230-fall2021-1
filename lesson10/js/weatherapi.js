72eed85ead222154c7a6e92e1eb43b8bcd9f491e
Dlyted1 <mookathy3@gmail.com>
2021-11-15 20:22:39 -0500

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/data/2.5/forecast?id=5604473&appid=86daac23ffd9b9bce45f9ec73da9e4e9";
+const apiURL = "http://api.openweathermap.org/data/data/2.5/weather?id=5604473&units=imperial&appid=86daac23ffd9b9bce45f9ec73da9e4e9";
 fetch(apiURL)
 .then((response) => response.json())
   .then((jsObject) => {
