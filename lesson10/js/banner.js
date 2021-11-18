d5325c527c65f96d8a6572daeaf38a173a77ff1e
Dlyted1 <mookathy3@gmail.com>
2021-11-18 17:38:22 -0500

    commit

---
+++ lesson10/js/banner.js
@@ -0,0 +1,16 @@
+
+function bannerFriday() {
+
+    const d = new Date();
+    const today = d.getDay();
+    const banner = document.querySelector(".banner");
+
+    if (today == 5) {
+        banner.small.display = "block";
+
+    } else {
+        banner.small.display = "none";
+    }
+}
+bannerFriday();
+
