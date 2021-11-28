d5d67be6b9fed39333d19ea0fda7cfbdfac07696
Dlyted1 <mookathy3@gmail.com>
2021-11-27 22:29:23 -0500

    commit

--- lesson11/js/eventspreston.js
+++ lesson11/js/eventspreston.js
@@ -1,4 +1,4 @@
-const townURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
   .then(function (response) {
