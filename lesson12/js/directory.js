d60bb61077cd87fbea7ea7f6cc73ac1e5c1f0902
Dlyted1 <mookathy3@gmail.com>
2021-12-15 14:44:34 -0500

    commit

--- lesson12/js/directory.js
+++ lesson12/js/directory.js
@@ -11,21 +11,26 @@ fetch(requestURL)
     let card = document.createElement("section");
     let h3 = document.createElement("h3");
     let address = document.createElement("p");
-    let place = document.createElement("p");
+    let phone = document.createElement("p");
     let image = document.createElement("img");
+    let url = document.createElement("a");
 
     h3.textContent = `${directory[i].name} ${directory[i].lastname}`;
-    address.textContent = `Date of Birth: ${directory[i].address}`;
-    place.textContent = `Place of Birth: ${directory[i].birthplace}`;
+    address.textContent = `${directory[i].address}`;
+    phone.textContent = `${directory[i].phone}`;
     image.setAttribute("src", directory[i].imageurl);
     image.setAttribute(
       "alt",
-      `${directory[i].name} ${directory[i].lastname}-${directory[i].order}`
+      `${directory[i].name} -${directory[i].order}`
     );
+    url.textContent = `${directory[i].url}`;
+    
+    card.appendChild(image);
     card.appendChild(h3);
     card.appendChild(address);
-    card.appendChild(place);
-    card.appendChild(image);
+    card.appendChild(phone);
+    card.appendChild(url);
+    
     document.querySelector("div.cards").appendChild(card);
   }
 });
