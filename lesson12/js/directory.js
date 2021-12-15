a5b1203d40b79deaf11f51f2107f749348572449
Dlyted1 <mookathy3@gmail.com>
2021-12-15 14:18:11 -0500

    commit

--- lesson12/js/directory.js
+++ lesson12/js/directory.js
@@ -1,77 +1,31 @@
 const requestURL = "https://dlyted1.github.io/wdd230-fall2021-1/lesson12/data/sebring-directory.json"
 
 fetch(requestURL)
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function (jsonObject) {
-    console.log(jsonObject);
+.then(function(response) {
+  return response.json()
+ }) // Convert data to json
+.then(function (data) {
+  console.log(data);
+  const directory = data["directory"];
+  for (let i = 0; i < directory.length; i++) {
+    let card = document.createElement("section");
+    let h3 = document.createElement("h3");
+    let address = document.createElement("p");
+    let place = document.createElement("p");
+    let image = document.createElement("img");
 
-    const directory = jsonObject["directory"];
-    for (let i = 0; i, i < directory length; i++) {
-
-      let card = document.createElement("section");
-      let name = document.createElement("h3");
-      let address = document.createElement("p");
-      let phone = document.createElement("p");
-      let url = document.createElement("a");
-      let image = document.createElement("img");
-
-      name.textContent = `${directory[i].name}`;
-      address.textContent = `${directory[i].address}`;
-      phone.textContent = `${directory[i].phone}`;
-      url.textContent = `${directory[i].url}`;
-      image.setAttribute("src", directory[i].imageurl);
-      
-      card.appendChild(name);
-      card.appendChild(address);
-      card.appendChild(phone);
-      card.appendChild(url);
-      card.appendChild(image);
-      document.querySelector("div.cards").appendChild(card);
-
-      const cards = document.querySelector(".cards");
-      cards.appendChild(card);
-      cards.classList.add("cards_view_grid");
-
-    }
-  });
-
-  function listView() {
-    const cards = document.querySelector(".cards");
-    const listBtn = document.querySelector(".btn_list");
-    const gridBtn = document.querySelector(".btn_grid");
-
-    cards.classList.remove("cards_view_grid");
-    cards.classList.add("cards_view_list");
-    gridBtn.classList.remove("active");
-    listBtn.classList.add("active");
-}
-
-function gridView() {
-    const cards = document.querySelector(".cards");
-    const listBtn = document.querySelector(".btn_list");
-    const gridBtn = document.querySelector(".btn_grid");
-
-    cards.classList.remove("cards_view_list");
-    cards.classList.add("cards_view_grid");
-    listBtn.classList.remove("active");
-    gridBtn.classList.add("active");
-}
-
-// for the scroll
-$(function() {
-    $('.scrollup').click(function() {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 1000);
-    })
-})
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 200) {
-        $(".scrollup").fadeIn();
-    } else {
-        $(".scrollup").fadeOut();
-    }
+    h3.textContent = `${directory[i].name} ${directory[i].lastname}`;
+    address.textContent = `Date of Birth: ${directory[i].address}`;
+    place.textContent = `Place of Birth: ${directory[i].birthplace}`;
+    image.setAttribute("src", directory[i].imageurl);
+    image.setAttribute(
+      "alt",
+      `${directory[i].name} ${directory[i].lastname}-${directory[i].order}`
+    );
+    card.appendChild(h3);
+    card.appendChild(address);
+    card.appendChild(place);
+    card.appendChild(image);
+    document.querySelector("div.cards").appendChild(card);
+  }
 });
